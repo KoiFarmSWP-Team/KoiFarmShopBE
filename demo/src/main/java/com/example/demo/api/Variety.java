b4e4c532317bbcd478c422cf8da4b43c7e4e47fb
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-10-08 23:52:56 +0700

    reset

--- demo/src/main/java/com/example/demo/api/Variety.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.api;
-
-import org.springframework.web.bind.annotation.PostMapping;
-
-public class Variety
-{
-    // add giong ca moi
-}
--- demo/src/main/java/com/example/demo/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Entity.Service;
-public interface ServiceRepository extends JpaRepository<Service, Long>{
-    Service findServiceById(long id);
-}
